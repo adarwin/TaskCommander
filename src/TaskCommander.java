d508ca573a67025b696c11abdcf4d333687fd219
apdmin <apdmin@gmail.com>
2012-10-20 14:25:07 -0400

    Added quick task addition feature

--- src/TaskCommander.java
+++ src/TaskCommander.java
@@ -43,58 +43,113 @@ public class TaskCommander
   private static void addComponentsToFrame(JFrame mainFrame)
   {
     Container contentPane = mainFrame.getContentPane();
-    /*
+    JPanel constantsPanel = new JPanel();
+    constantsPanel.setLayout(new BorderLayout());
+
     JMenuBar menuBar = new JMenuBar();
-    menuBar.setVisible(true);
-    mainFrame.add(menuBar);
-    */
+    //Build the file menu
+      JMenu fileMenu = new JMenu("File");
+      fileMenu.setMnemonic(KeyEvent.VK_F);
+      JMenuItem menuItem = new JMenuItem("New...", KeyEvent.VK_N);
+      fileMenu.add(menuItem);
+      menuItem = new JMenuItem("Exit", KeyEvent.VK_X);
+      menuItem.addActionListener(new ActionListener()
+      {
+        public void actionPerformed(ActionEvent e)
+        {
+          System.exit(0);
+        }
+      });
+      fileMenu.addSeparator();
+      fileMenu.add(menuItem);
+
+    //Build the edit menu
+      JMenu editMenu = new JMenu("Edit");
+      editMenu.setMnemonic(KeyEvent.VK_E);
+      menuItem = new JMenuItem("Undo", KeyEvent.VK_U);
+      menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Z, InputEvent.META_DOWN_MASK));
+      menuItem.addActionListener(new ActionListener()
+      {
+        public void actionPerformed(ActionEvent e)
+        {
+          undo();
+          taskEntryPanel.revalidate();
+          taskEntryPanel.repaint();
+        }
+      });
+      editMenu.add(menuItem);
+      menuItem = new JMenuItem("Redo", KeyEvent.VK_R);
+      menuItem.setAccelerator(KeyStroke.getKeyStroke(KeyEvent.VK_Y, InputEvent.META_DOWN_MASK));
+      menuItem.addActionListener(new ActionListener()
+      {
+        public void actionPerformed(ActionEvent e)
+        {
+          redo();
+          taskEntryPanel.revalidate();
+          taskEntryPanel.repaint();
+        }
+      });
+      editMenu.add(menuItem);
+
+    //Build the view menu
+      JMenu viewMenu = new JMenu("View");
+      viewMenu.setMnemonic(KeyEvent.VK_V);
+
+      ButtonGroup viewModes = new ButtonGroup();
+      JRadioButtonMenuItem rbMenuItem = new JRadioButtonMenuItem("Task Entry Mode");
+      rbMenuItem.setSelected(true);
+      rbMenuItem.setMnemonic(KeyEvent.VK_T);
+      viewModes.add(rbMenuItem);
+      viewMenu.add(rbMenuItem);
+      rbMenuItem = new JRadioButtonMenuItem("Planning Board");
+      rbMenuItem.setMnemonic(KeyEvent.VK_P);
+      viewModes.add(rbMenuItem);
+      viewMenu.add(rbMenuItem);
+
+    //Build the tools menu
+      JMenu toolsMenu = new JMenu("Tools");
+      toolsMenu.setMnemonic(KeyEvent.VK_T);
+      JMenu macrosMenu = new JMenu("Macros");
+      macrosMenu.setMnemonic(KeyEvent.VK_M);
+      menuItem = new JMenuItem("Record");
+      macrosMenu.add(menuItem);
+      menuItem = new JMenuItem("Manage...");
+      macrosMenu.add(menuItem);
+      toolsMenu.add(macrosMenu);
+
+
+    menuBar.add(fileMenu);
+    menuBar.add(editMenu);
+    menuBar.add(viewMenu);
+    menuBar.add(toolsMenu);
+
+
+    JToolBar toolBar = new JToolBar();
+    toolBar.setFloatable(false);
+    toolBar.setBorderPainted(true);
+    //toolBar.setPreferredSize(new Dimension(100, 50));
+    toolBar.add(new JButton("ToolbarButton"));
+
+
     taskEntryPanel = new TaskEntryPanel();
-    contentPane.add(taskEntryPanel, BorderLayout.CENTER);
-    JButton create = new JButton("Create New Task");
     JButton undo = new JButton("Undo");
     JButton redo = new JButton("Redo");
     input = new JTextField();
-    contentPane.add(create, BorderLayout.SOUTH);
-    contentPane.add(undo, BorderLayout.WEST);
-    contentPane.add(redo, BorderLayout.EAST);
-    contentPane.add(input, BorderLayout.NORTH);
+
+    constantsPanel.add(menuBar, BorderLayout.NORTH);
+    constantsPanel.add(toolBar, BorderLayout.SOUTH);
+    contentPane.add(constantsPanel, BorderLayout.NORTH);
+    contentPane.add(taskEntryPanel, BorderLayout.CENTER);
 
 
 
-    create.addActionListener(new ActionListener()
-    {
-      public void actionPerformed(ActionEvent e)
-      {
-        addTask();
-        taskEntryPanel.revalidate();
-        taskEntryPanel.repaint();
-      }
-    });
-    undo.addActionListener(new ActionListener()
-    {
-      public void actionPerformed(ActionEvent e)
-      {
-        undo();
-        taskEntryPanel.revalidate();
-        taskEntryPanel.repaint();
-      }
-    });
-    redo.addActionListener(new ActionListener()
-    {
-      public void actionPerformed(ActionEvent e)
-      {
-        redo();
-        taskEntryPanel.revalidate();
-        taskEntryPanel.repaint();
-      }
-    });
   }
 
   //Public Methods
     public static JFrame getFrame() { return frame; }
     public static void undo()
     {
-      if (!runCommands.isEmpty())
+      if (runCommands != null && !runCommands.isEmpty())
         {
         Command lastCommand = runCommands.pop();
         lastCommand.undo();
@@ -108,7 +163,7 @@ public class TaskCommander
   
     public static void redo()
     {
-      if(!undonCommands.isEmpty())
+      if(undonCommands != null && !undonCommands.isEmpty())
       {
         Command lastCommand = undonCommands.pop();
         lastCommand.redo();
@@ -143,6 +198,9 @@ public class TaskCommander
       Command command = new TaskAddition(taskEntryPanel, new TaskWidget(name));
       command.run();
       addCommand(command);
+
+      taskEntryPanel.revalidate();
+      taskEntryPanel.repaint();
     }
 
 
--- src/TaskEntryPanel.java
+++ src/TaskEntryPanel.java
@@ -5,9 +5,12 @@ CSC 420: Graphical User Interfaces
 SUNY Oswego
 */
 
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 import java.awt.BorderLayout;
 import java.awt.Component;
 import java.awt.Dimension;
+import java.awt.Insets;
 import java.util.Arrays;
 import java.util.List;
 import javax.swing.border.TitledBorder;
@@ -16,6 +19,7 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JSplitPane;
+import javax.swing.JTextField;
 import javax.swing.SpringLayout;
 
 public class TaskEntryPanel extends JPanel
@@ -23,39 +27,118 @@ public class TaskEntryPanel extends JPanel
   JSplitPane mainSplitPane;
   JSplitPane nestedSplitPane;
   JPanel leftContent, centerContent, rightContent;
-  SpringLayout layout;
+  JPanel leftPane, centerPane, rightPane;
+  SpringLayout layout, centerLayout;
+  JTextField quickTaskField;
+  JButton taskAddButton;
+  JScrollPane leftScrollPane, centerScrollPane, rightScrollPane;
 
   public TaskEntryPanel()
   {
     super();
-    mainSplitPane = new JSplitPane();
-    nestedSplitPane = new JSplitPane();
-    leftContent = new JPanel();
-    centerContent = new JPanel();
-    rightContent = new JPanel();
+    // Initialize Components and Containers
+      // Split Panes
+        mainSplitPane = new JSplitPane();
+        nestedSplitPane = new JSplitPane();
 
-    leftContent.setLayout(new BoxLayout(leftContent, BoxLayout.PAGE_AXIS));
-    centerContent.setLayout(new BoxLayout(centerContent, BoxLayout.PAGE_AXIS));
-    rightContent.setLayout(new BoxLayout(rightContent, BoxLayout.PAGE_AXIS));
-    JScrollPane centerScrollPane = new JScrollPane(centerContent);
-    centerScrollPane.setMinimumSize(new Dimension(300, 0));
+      // Panels
+        leftContent = new JPanel();
+        centerContent = new JPanel();
+        rightContent = new JPanel();
+        leftPane = new JPanel();
+        centerPane = new JPanel();
+        rightPane = new JPanel();
 
-    mainSplitPane.setRightComponent(nestedSplitPane);
-    mainSplitPane.setLeftComponent(new JScrollPane(leftContent));
-    nestedSplitPane.setLeftComponent(centerScrollPane);
-    nestedSplitPane.setRightComponent(new JScrollPane(rightContent));
+      // Scroll Panes
+        leftScrollPane = new JScrollPane(leftContent);
+        centerScrollPane = new JScrollPane(centerContent);
+        rightScrollPane = new JScrollPane(rightContent);
+
+      // Other Components
+        quickTaskField = new JTextField();
+        taskAddButton = new JButton("+");
+
+      // Layouts
+        centerLayout = new SpringLayout();
+        layout = new SpringLayout();
 
 
-    mainSplitPane.setContinuousLayout(true);
-    nestedSplitPane.setContinuousLayout(true);
+    //Configure Components
+      taskAddButton.setPreferredSize(new Dimension(20, 20));
+      quickTaskField.setText("Enter new task here");
+      centerScrollPane.setMinimumSize(new Dimension(300, 0));
 
-    add(mainSplitPane);
-    layout = new SpringLayout();
-    layout.putConstraint(SpringLayout.NORTH, mainSplitPane, 0, SpringLayout.NORTH, this);
-    layout.putConstraint(SpringLayout.SOUTH, mainSplitPane, 0, SpringLayout.SOUTH, this);
-    layout.putConstraint(SpringLayout.EAST, mainSplitPane, 0, SpringLayout.EAST, this);
-    layout.putConstraint(SpringLayout.WEST, mainSplitPane, 0, SpringLayout.WEST, this);
-    setLayout(layout);
+
+    //Configure Containers
+      mainSplitPane.setRightComponent(nestedSplitPane);
+      mainSplitPane.setLeftComponent(leftScrollPane);
+      nestedSplitPane.setLeftComponent(centerPane);
+      nestedSplitPane.setRightComponent(rightScrollPane);
+
+      mainSplitPane.setContinuousLayout(true);
+      nestedSplitPane.setContinuousLayout(true);
+
+
+    //Add components
+      centerPane.add(quickTaskField);
+      centerPane.add(taskAddButton);
+      centerPane.add(centerScrollPane);
+      add(mainSplitPane);
+
+
+    //Set Layouts
+      setLayout(layout);
+      leftContent.setLayout(new BoxLayout(leftContent, BoxLayout.PAGE_AXIS));
+      centerContent.setLayout(new BoxLayout(centerContent, BoxLayout.PAGE_AXIS));
+      rightContent.setLayout(new BoxLayout(rightContent, BoxLayout.PAGE_AXIS));
+      centerPane.setLayout(centerLayout);
+
+
+    //Configure Layouts
+      centerLayout.putConstraint(SpringLayout.NORTH, taskAddButton, 0, SpringLayout.NORTH, quickTaskField);
+      centerLayout.putConstraint(SpringLayout.SOUTH, taskAddButton, 0, SpringLayout.SOUTH, quickTaskField);
+      centerLayout.putConstraint(SpringLayout.EAST, taskAddButton, 0, SpringLayout.EAST, centerPane);
+    
+      centerLayout.putConstraint(SpringLayout.NORTH, quickTaskField, 0, SpringLayout.NORTH, centerPane);
+      centerLayout.putConstraint(SpringLayout.EAST, quickTaskField, 0, SpringLayout.WEST, taskAddButton);
+      centerLayout.putConstraint(SpringLayout.WEST, quickTaskField, 0, SpringLayout.WEST, centerPane);
+
+
+      centerLayout.putConstraint(SpringLayout.NORTH, centerScrollPane, 0, SpringLayout.SOUTH,
+                                 quickTaskField.getPreferredSize().height > taskAddButton.getPreferredSize().height ? quickTaskField : taskAddButton);
+      centerLayout.putConstraint(SpringLayout.SOUTH, centerScrollPane, 0, SpringLayout.SOUTH, centerPane);
+      centerLayout.putConstraint(SpringLayout.EAST, centerScrollPane, 0, SpringLayout.EAST, centerPane);
+      centerLayout.putConstraint(SpringLayout.WEST, centerScrollPane, 0, SpringLayout.WEST, centerPane);
+
+
+      layout.putConstraint(SpringLayout.NORTH, mainSplitPane, 0, SpringLayout.NORTH, this);
+      layout.putConstraint(SpringLayout.SOUTH, mainSplitPane, 0, SpringLayout.SOUTH, this);
+      layout.putConstraint(SpringLayout.EAST, mainSplitPane, 0, SpringLayout.EAST, this);
+      layout.putConstraint(SpringLayout.WEST, mainSplitPane, 0, SpringLayout.WEST, this);
+
+
+
+    //Add Action Listeners
+      quickTaskField.addActionListener(new ActionListener()
+      {
+        public void actionPerformed(ActionEvent e)
+        {
+          TaskCommander.addTask(quickTaskField.getText());
+        }
+      });
+      taskAddButton.addActionListener(new ActionListener()
+      {
+        public void actionPerformed(ActionEvent e)
+        {
+          TaskCommander.addTask(quickTaskField.getText());
+        }
+      });
+  }
+
+
+  public String getQuickTaskFieldText()
+  {
+    return quickTaskField.getText();
   }
 
 
