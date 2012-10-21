479e42efbb0736f283900da0a4c0e96635780ba0
apdmin <apdmin@gmail.com>
2012-10-20 20:03:31 -0400

    Setup very basic structure

--- src/TaskPlanningPanel.java
+++ src/TaskPlanningPanel.java
@@ -6,7 +6,45 @@ SUNY Oswego
 */
 
 import javax.swing.JPanel;
+import javax.swing.JSplitPane;
 
-public class TaskPlanningPanel extends JPanel
+public class TaskPlanningPanel extends JPanel implements TaskView
 {
+  JSplitPane mainSplitPane;
+  JPanel unPlannedContent, plannedContent;
+  PlanningCalendar calendar;
+
+  public TaskPlanningPanel()
+  {
+    super();
+    // Initialize Components and Containers
+      // Panels
+        unPlannedContent = new JPanel();
+        plannedContent = new JPanel();
+      calendar = new PlanningCalendar();
+      mainSplitPane = new JSplitPane();
+
+    // Configure Containers
+      mainSplitPane.setLeftComponent(unPlannedContent);
+      mainSplitPane.setRightComponent(plannedContent);
+      mainSplitPane.setContinuousLayout(true);
+      mainSplitPane.setDividerLocation(TaskCommander.startingWidth/3);
+      setLayout(new BorderLayout());
+
+    // Add components
+      add(mainSplitPane, BorderLayout.CENTER);
+  }
+
+  public void addCourse(Course course)
+  {
+  }
+  public void removeCourse(Course course)
+  {
+  }
+  public void addTask(Task task)
+  {
+  }
+  public void removeTask(Task task)
+  {
+  }
 }
